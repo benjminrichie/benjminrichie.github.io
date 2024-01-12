23c856ba5d073baecd1e32ba281f5c74a6e55076
Benjamin Richard <88556831+benjminrichie@users.noreply.github.com>
2024-01-12 08:17:38 +0100

    Add files via upload
    my portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const button = document.querySelector('#myBtn');
+button.addEventListener('click', () => {
+    alert('No data Base Configured Yet!!!');
+})
+
+
+const suButton = document.querySelector('#subscribe');
+suButton.addEventListener('keyup', () => {
+    console.log('Event Triggered');
+})
+
+
+const list = document.querySelectorAll('li');
+list
